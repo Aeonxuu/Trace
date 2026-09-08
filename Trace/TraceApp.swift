f7d0ee121116931afda3a4eb2e65ba72654a98ed
Aeonxuu <lalainejannahdelacruz@gmail.com>
2026-09-08 20:38:32 +0800

    Initial Commit

---
+++ Trace/TraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TraceApp.swift
+//  Trace
+//
+//  Created by LJ on 9/8/26.
+//
+
+import SwiftUI
+
+@main
+struct TraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
